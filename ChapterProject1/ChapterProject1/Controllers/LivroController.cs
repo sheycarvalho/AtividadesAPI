d8193d1048dd1a34a8b0844651ae6cdf12e9e9cb
sheyenne de morais carvalho <sheyenne_22@hotmail.com>
2022-09-01 19:38:54 -0400

    ProjetoAPI - ER2

--- ChapterProject1/ChapterProject1/Controllers/LivroController.cs
+++ ChapterProject1/ChapterProject1/Controllers/LivroController.cs
@@ -1,4 +1,5 @@
-﻿using ChapterProject1.Repositories;
+﻿using ChapterProject1.Models;
+using ChapterProject1.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -30,6 +31,79 @@ namespace ChapterProject1.Controllers
             }
         }
 
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+            try
+            {
+                Livro livroBuscado = _livroRepository.BuscarPorId(id);
 
+                if (livroBuscado == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(livroBuscado);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+
+
+
+        [HttpPost]
+        public IActionResult Cadastrar(Livro l)
+        {
+            try
+            {
+                _livroRepository.Cadastrar(l);
+
+                return StatusCode(201);
+                //return Ok("Livro Cadastrado com sucesso");
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+
+
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            try
+            {
+                _livroRepository.Deletar(id);
+
+                return Ok("Livro removido com sucesso");
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+
+
+        [HttpPut("{id}")]
+        public IActionResult Alterar(int id, Livro l)
+        {
+            try
+            {
+                _livroRepository.Alterar(id, l);
+
+                return StatusCode(204);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
     }
 }
+
+
--- ChapterProject1/ChapterProject1/Repositories/LivroRepository.cs
+++ ChapterProject1/ChapterProject1/Repositories/LivroRepository.cs
@@ -18,5 +18,47 @@ namespace ChapterProject1.Repositories
         }
 
 
+
+        public Livro BuscarPorId(int id)
+        {
+            return _context.Livros.Find(id);
+        }
+
+
+        public void Cadastrar(Livro l)
+        {
+            _context.Livros.Add(l);
+
+            _context.SaveChanges();
+        }
+
+
+        public void Deletar(int id)
+        {
+             Livro l = _context.Livros.Find(id);
+
+            _context.Livros.Remove(l);
+
+            _context.SaveChanges();
+        }
+
+
+
+        public void Alterar(int id, Livro l)
+        {
+            Livro livroBuscado = _context.Livros.Find(id);
+
+            if (livroBuscado != null)
+            {
+                livroBuscado.Titulo = l.Titulo;
+                livroBuscado.QuantidadePaginas = l.QuantidadePaginas;
+                livroBuscado.Disponivel = l.Disponivel;
+
+                _context.Livros.Update(livroBuscado);
+
+                _context.SaveChanges();
+            }
+
+        }
     }
 }
